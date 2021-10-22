eab0f2da42fd7fc00156dc0ea0eccac7620b3075
Andres <agomez.vg@gmail.com>
2021-10-22 11:32:00 -0300

    cambios

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const baseURL = "http://localhost:3001/api/v1/restaurants";
-const baseURL = "https://restaurant-pern.herokuapp.com/";
+const baseURL = "https://restaurant-pern.herokuapp.com/api/v1/restaurants";
 export default axios.create({
   baseURL,
 });
